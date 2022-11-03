ed68d2ca720205ea330aa7580da379881d563823
fredrick Kebaso <fredrickkebaso@gmail.com>
2022-11-03 17:21:39 +0300

    Added quality Analysis code

--- Scripts/01_qualitycheck.sh
+++ Scripts/01_qualitycheck.sh
@@ -1,11 +1,19 @@
 #!/bin/bash env
 
-#Checks the quality of the reads using fastqc tool
+#Checks the quality of the reads using fastqc tool version 
+
+mkdir -p ${PWD}/Results/quality_reports
 
 for i in Data/*.gz
 
 do
 
-fastqc -o Results/quality_reports $i
+fastqc -o ${PWD}/Results/quality_reports 
 
 done;
+
+#General Report using Multiqc version
+
+mkdir -p ${PWD}/Results/multiqc_report
+
+multiqc -o ${PWD}/Results/multiqc_report ${PWD}/Results/quality_reports
