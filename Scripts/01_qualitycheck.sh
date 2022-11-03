b052dc77399e588482c1c5212c5dcc35b2ff8ae1
Fredrick Kebaso <fredrickkebaso@gmail.com>
2022-11-03 17:26:20 +0300

    Update 01_qualitycheck.sh

--- Scripts/01_qualitycheck.sh
+++ Scripts/01_qualitycheck.sh
@@ -1,6 +1,7 @@
 #!/bin/bash env
 
-#Checks the quality of the reads using fastqc tool version 
+#Checks the quality of the reads using fastqc tool version 0.11.9
+
 
 mkdir -p ${PWD}/Results/quality_reports
 
@@ -12,7 +13,7 @@ fastqc -o ${PWD}/Results/quality_reports
 
 done;
 
-#General Report using Multiqc version
+#General Report using Multiqc version 1.9
 
 mkdir -p ${PWD}/Results/multiqc_report
 
