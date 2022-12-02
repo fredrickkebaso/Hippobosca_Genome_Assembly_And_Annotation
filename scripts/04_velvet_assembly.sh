1e3f7ea27319e070d9d079daf32122d087aecd45
fredrick Kebaso <fredrickkebaso@gmail.com>
2022-12-02 08:55:15 +0300

    Added quast script

--- scripts/04_velvet_assembly.sh
+++ scripts/04_velvet_assembly.sh
@@ -4,13 +4,13 @@ echo Concatenating reads
 
 mkdir -p ${PWD}/results/{hvariegata_merged,velvet_out}
 
-for f_read in ${PWD}/results/clean_reads/*S2*_val_1.fq.gz
+for f_read in ${PWD}/results/clean_reads/*S1*_val_1.fq.gz
 
 do 
     RevRead=${f_read/_R1_001.fastq.gz/*_R2_002.fastq.gz}
 
-    cat $f_read >> ${PWD}/results/hvariegata_merged/S2-hvariegata_merged_R1_.fq.gz
-    cat $f_read >> ${PWD}/results/hvariegata_merged/S2-hvariegata_merged_R2_.fq.gz
+    cat $f_read >> ${PWD}/results/hvariegata_merged/S1-hvariegata_merged_R1_.fq.gz
+    cat $f_read >> ${PWD}/results/hvariegata_merged/S1-hvariegata_merged_R2_.fq.gz
 
 done;
 echo Concatenating reads Completed successfully !!!
@@ -19,7 +19,7 @@ echo Running velveth...Hashing...
 
 module load velvet
 
-velveth ${PWD}/results/velvet_out 21 -fastq.gz -shortPaired -separate ${PWD}/results/hvariegata_merged/S2-hvariegata_merged_R1_.fq.gz ${PWD}/results/hvariegata_merged/S2-hvariegata_merged_R2_.fq.gz 
+velveth ${PWD}/results/velvet_out 21 -fastq.gz -shortPaired -separate ${PWD}/results/hvariegata_merged/S1-hvariegata_merged_R1_.fq.gz ${PWD}/results/hvariegata_merged/S1-hvariegata_merged_R2_.fq.gz 
 
  echo Completed hashing successfully
   
