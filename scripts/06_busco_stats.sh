9540a96ea2fd06475b7a10256639f1665679e89f
fredrick Kebaso <fredrickkebaso@gmail.com>
2022-12-06 11:35:45 +0300

    Added Busco script

---
+++ scripts/06_busco_stats.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#For assessment of assembly quality
+
+busco \
+--in /home/kebaso/Documents/projects/hippo/results/velvet_out/contigs.fa \
+--lineage diptera_odb10 \
+--out genome_stats \
+--out_path /home/kebaso/Documents/projects/hippo/results/busco_stats/ \
+--mode genome \
+--augustus \
+--cpu 2 \
+--download_path /home/kebaso/Documents/projects/hippo/results/busco_stats/ \
+--force \
+--scaffold_composition \
+--update-data
