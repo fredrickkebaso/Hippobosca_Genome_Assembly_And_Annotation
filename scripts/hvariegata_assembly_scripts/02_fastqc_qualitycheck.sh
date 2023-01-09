1418b2dce43a964808ecea92f7a6f7305559f0f8
fredrick Kebaso <fredrickkebaso@gmail.com>
2023-01-09 11:43:48 +0300

    Added scripts

--- scripts/hvariegata_assembly_scripts/02_fastqc_qualitycheck.sh
+++ scripts/hvariegata_assembly_scripts/02_fastqc_qualitycheck.sh
@@ -8,13 +8,13 @@ echo Checks the quality of the reads using fastqc tool version 0.11.9
 
 echo Found version `fastqc --version`
 
-mkdir -p results/quality_reports/{quality_raw_reads,multiqc_raw_reads}
+mkdir -p results/quality_reports/fastqc_raw_reads,multiqc_raw_reads
 
-fastqc data/merged_raw_reads/*.fastq.gz -t 8 -o results/quality_reports/quality_raw_reads
+fastqc data/merged_raw_reads/*.fastq.gz -t 8 -o results/quality_reports/fastqc_raw_reads
 
 #General Report using Multiqc version 1.11
 
 echo Generating multiqc report multiqc version 1.11
 
-multiqc -o results/quality_reports/multiqc_raw_reads results/quality_reports/quality_raw_reads
+multiqc -o results/quality_reports/multiqc_raw_reads results/quality_reports/fastqc_raw_reads
 
--- scripts/hvariegata_assembly_scripts/03_trim_galore_parallel.sh
+++ scripts/hvariegata_assembly_scripts/03_trim_galore_parallel.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 set -eu
 
@@ -6,37 +6,27 @@ echo "Cleaning data using trim_galore v 0.6.6 ..."
 
 echo Found version `trim_galore --version`
 
-mkdir -p results/quality_reports/{quality_clean_reads,multiqc_clean_reads}
+mkdir -p results/quality_reports/{fastqc_clean_reads,multiqc_clean_reads}
 
-echo "Cleaning data using trim_galore in a parallel mode..."
-
-ls -1 data/merged_raw_reads/*.fastq.gz | \
-cut -d. -f1 | sort | uniq | \
-parallel -j 2 \
 trim_galore \
---paired {}.R1.fastq.gz {}.R2.fastq.gz \
+--paired data/merged_raw_reads/*.R1.fastq.gz data/merged_raw_reads/*.R2.fastq.gz \
 --cores 8 \
 --length 1 \
 --phred33 \
 --nextera \
+--fastqc_args "--outdir results/quality_reports/fastqc_clean_reads" \
 --output_dir results/clean_reads
 
 echo Done.
 
-echo Checking the quality of the reads using fastqc tool version 0.11.9
-
-echo Found version `fastqc --version`
-
-echo Evaluating the quality of the cleaned reads...
-
-fastqc results/clean_reads/*.gz -t 8 -o results/quality_reports/quality_clean_reads
-
 echo Generating report using Multiqc version 1.11
 
 
-multiqc -o results/quality_reports/multiqc_clean_reads results/quality_reports/quality_clean_reads
+multiqc -o results/quality_reports/multiqc_clean_reads results/quality_reports/fastqc_clean_reads
 
 echo Done.
+
+
 
 
 
